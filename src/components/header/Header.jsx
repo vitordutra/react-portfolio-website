7c26f5631f28e720431095777b0946b0aec49d70
Vitor Dutra <jo.vitor.d@gmail.com>
2022-03-19 20:30:10 -0300

    Add Social Icons & Links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './header.css';
 import CTA from './CTA';
 import ME from '../../assets/me.png';
+import HeaderSocial from './HeaderSocial';
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
         <h1>Vitor Dutra</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
         <CTA />
+        <HeaderSocial />
 
         <div className='me'>
           <img src={ME} alt='me' />
