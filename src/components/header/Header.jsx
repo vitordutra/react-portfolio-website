20cce885944c236a2d595f367405e627919a70de
Vitor Dutra <jo.vitor.d@gmail.com>
2022-03-19 20:22:44 -0300

    Header Component JSX

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/me.png';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Vitor Dutra</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+
+        <div className='me'>
+          <img src={ME} alt='me' />
+        </div>
+
+        <a href='#contact' className='scroll__down'>
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
