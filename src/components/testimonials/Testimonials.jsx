c6e9a56ba9c1cc5fdfecf85ffceca5bd602cde2c
Vitor Dutra <jo.vitor.d@gmail.com>
2022-03-23 09:46:22 -0300

    Generate Testimonials Using array.map

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,33 @@ import AVTR2 from '../../assets/avatar2.jpg';
 import AVTR3 from '../../assets/avatar3.jpg';
 import AVTR4 from '../../assets/avatar4.jpg';
 
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review:
+      'Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!A ordem dos tratores não altera o pão duris.Delegadis gente finis, bibendum egestas augue arcu ut est.',
+  },
+  {
+    avatar: AVTR2,
+    name: 'Shatta Wale',
+    review:
+      'Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!A ordem dos tratores não altera o pão duris.Delegadis gente finis, bibendum egestas augue arcu ut est.',
+  },
+  {
+    avatar: AVTR3,
+    name: 'Kwame Despite',
+    review:
+      'Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!A ordem dos tratores não altera o pão duris.Delegadis gente finis, bibendum egestas augue arcu ut est.',
+  },
+  {
+    avatar: AVTR4,
+    name: 'Nana Ama McBrown',
+    review:
+      'Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!A ordem dos tratores não altera o pão duris.Delegadis gente finis, bibendum egestas augue arcu ut est.',
+  },
+];
+
 const Testimonials = () => {
   return (
     <section id='testimonials'>
@@ -12,18 +39,17 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
 
       <div className='container testimonials__container'>
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVTR1} alt='Avatar One' />
-          </div>
-          <h5 className='client__name'>Ernest Achiever</h5>
-          <small className='client__review'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
-            placeat et harum voluptate modi obcaecati repellat aliquid
-            recusandae doloribus iure tempora ex, voluptates minus ullam illum
-            consectetur sint, fuga reiciendis.
-          </small>
-        </article>
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <article key={index} className='testimonial'>
+              <div className='client__avatar'>
+                <img src={avatar} alt='Avatar One' />
+              </div>
+              <h5 className='client__name'>{name}</h5>
+              <small className='client__review'>{review}</small>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
